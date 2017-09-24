8b7c872284aa76bb30b5227ca07bf355356165db
Adam Plotzker <adamplotzker@Adams-MacBook-Air.local>
2017-09-24 14:51:23 -0400

    Adding Define Host Pop-up View Template

---
+++ hostit/DefineHostViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DefineHostViewController.swift
+//  hostit
+//
+//  Created by Adam Plotzker on 9/24/17.
+//  Copyright © 2017 Adam Plotzker. All rights reserved.
+//
+
+import UIKit
+
+class DefineHostViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func exitHostView(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ hostit/pinHostMenu.swift
@@ -0,0 +1,12 @@
+//
+//  PinHostMenu.swift
+//  hostit
+//
+//  Created by Adam Plotzker on 9/24/17.
+//  Copyright © 2017 Adam Plotzker. All rights reserved.
+//
+
+import Foundation
+
+
+
